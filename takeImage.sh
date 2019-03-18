9b4b9adbc8e84dd54a425e3b76fde64c0a64a7a1
Hamid Zare <dellydela@gmail.com>
2019-03-18 10:43:25 -0500

    fixed takeImage stderr default output and location

--- takeImage.sh
+++ takeImage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 date_str=`date +%y-%m-%d--%H-%M-%S`
+dataset_path=$1
 echo taking an image $(date)
-fswebcam -r 1920x1080 -S 15 $HOME/storage/datasets/webcam/myface/lab_$date_str.jpg
+fswebcam -r 1920x1080 -S 15 $dataset_path/lab_$date_str.jpg 2>&1
