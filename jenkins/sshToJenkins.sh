327e941b90daa2dcf0d8a1b605f5a631706e1ea4
Hamid Zare <dellydela@gmail.com>
2020-04-09 14:18:11 -0700

    jenkins ssh script updates

---
+++ jenkins/sshToJenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ssh into a running jenkins instance
+
+ssh_keypair_path=$1
+instance_id=$2
+
+ssh -i $ssh_keypair_path  -o StrictHostKeyChecking=no ubuntu@$(awsInstanceIp.sh $instance_id)
--- jenkins/sshToPr.sh
+++ jenkins/sshToPr.sh
@@ -9,6 +9,9 @@ pr=$4
 
 pr_dir="workspace/${test_name}_PR-${pr}"
 
+echo "issue the following to move to the target directory"
+echo "cd ${pr_dir}"
 ssh -i $ssh_keypair_path  -o StrictHostKeyChecking=no \
-  ubuntu@$(awsInstanceIp.sh $instance_id) "cd ${pr_dir}; echo 'connected'; sh"
+  ubuntu@$(awsInstanceIp.sh $instance_id)
+  # ubuntu@$(awsInstanceIp.sh $instance_id) "cd ${pr_dir}; echo 'connected'; sh"
 # TODO remove the need for `sh`: don't exit after running the command.
