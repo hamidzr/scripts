77ec5b07fc5705eef7afb195a8dda664db478ac9
Hamid Zare <dellydela@gmail.com>
2020-04-21 15:07:07 -0700

    update the jenkins url parsing to better capture instance id

--- jenkins/parse-jenkins-url.sh
+++ jenkins/parse-jenkins-url.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 url=$1
 get_instance_id=${2:-true}
@@ -14,7 +14,7 @@ run=$(echo $url | grep -Po 'PR-[^/]+/\K[^/]*')
 # also get jenkins instance id
 # Jenkins (i-0064686a1002f21b6) (executor: 0)
 if $get_instance_id; then
-  instance_id=$(jenkins-logs.sh $pipline_name $pr $run | grep -Po -m 1 'Jenkins \(\K[^\)]+')
+  instance_id=$(jenkins-logs.sh $pipline_name $pr $run | grep -Po -m 1 '\(\Ki-[\w]{6,18}')
 fi
 
 echo "$instance_id $pipline_name $pr $run" | sed 's/^ *//;s/ *$//'
