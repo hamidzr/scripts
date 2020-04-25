f8d57f3f14b49fac13ed6d13212ce61fe3c3a774
Hamid Zare <dellydela@gmail.com>
2020-04-25 14:50:33 -0700

    ffmpeg wc-to-wc script

---
+++ ffmpeg/wc-to-wc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+src=$1
+dst=$2
+
+fps=20
+
+# crop filter options
+dim_w=350
+dim_h=350
+offset_w=164
+offset_h=54
+
+
+
+# -f video4linux2 -input_format h264 -video_size 640x480 -framerate 20 \
+
+ffmpeg \
+  -f video4linux2 -input_format rawvideo -video_size 640x480 -framerate 30 \
+  -i $src \
+  -filter:v "crop=$dim_w:$dim_h:$offset_w:$offset_h" \
+  -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 $dst
+
+
