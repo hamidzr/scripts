b9d15d2083781feb3501bcb22973ad5c05431d37
Hamid Zare <dellydela@gmail.com>
2020-05-05 20:23:46 -0700

    options for vertical and horizontal

--- ffmpeg/wc-to-wc.sh
+++ ffmpeg/wc-to-wc.sh
@@ -2,24 +2,31 @@
 
 src=$1
 dst=$2
+orientation=${3:-vertical}
 
 fps=20
 
-## crop filter options
-# square
-# dim_w=350
-# dim_h=350
-
-# vertical
-dim_w=240
-dim_h=320
-
-# horizontal
-# dim_w=320
-# dim_h=240
-
 offset_w=164
 offset_h=54
+
+## crop filter options
+if [[ "${orientation}" == "vertical" ]]; then
+  # vertical
+  dim_w=240
+  dim_h=360
+  offset_w=164
+  offset_h=54
+elif [[ "${orientation}" == "horizontal"  ]]; then
+  # horizontal
+  dim_w=360
+  dim_h=240
+  offset_w=100
+  offset_h=100
+else
+  echo "unsupported orientation"
+  exit 1
+fi
+
 
 
 
