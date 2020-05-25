8708447c5ff60dae720f9a487713692c47998291
Hamid Zare <dellydela@gmail.com>
2020-05-25 14:11:29 -0700

    add more resolution options

--- ffmpeg/window-to-webcam.sh
+++ ffmpeg/window-to-webcam.sh
@@ -21,21 +21,22 @@ xo=$(echo "$window_info" | grep -i 'absolute.*x:' | grep -Po '\d+')
 yo=$(echo "$window_info" | grep -i 'absolute.*y:' | grep -Po '\d+')
 
 
-declare typical_resolutions=("4096x2160
-3840x2160
-2048x1080
+declare resolution_opts=("2560x1440
 1920x1080
+1600x900
 1280x720
-1024x768
-800x600
-720x486
-720x480
-720x576
-618x618
-408x408
-300x300")
+1024x576
+854x480
+640x360
+720x1280
+480x854
+360x640
+500x500
+300x300
+input")
 
-outres=$(echo -e "${typical_resolutions[@]}" | dmenu -i -p 'pick output resolution')
+outres=$(echo -e "${resolution_opts[@]}" | dmenu -i -p 'pick output resolution')
+[[ "${outres}" == "input" ]] && outres=$inres
 
 echo "sizing: ($inres, $xo, $yo, $outres)"
 ffmpeg -f x11grab -show_region 1 -r $fps -s $inres -i :0.0+$xo,$yo -vcodec rawvideo -pix_fmt yuv420p -s $outres -threads 0 \
