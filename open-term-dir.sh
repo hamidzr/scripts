1d34cbf8cdc70d15de560d2f9f0e1df833dfb8bb
Hamid Zare <dellydela@gmail.com>
2020-05-26 22:37:46 -0700

    add more comments around open-term-dir

--- open-term-dir.sh
+++ open-term-dir.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-# a compatible way of opening various terminals in a target dir
+# open a directory in your terminal emulator
+# $TERMINAL -e "cd ${dir} && zsh"
 
+# a more widely supported way of opening various terminals in a target dir
 $TERMINAL -e "sh -c 'cd $1 && $SHELL'"
