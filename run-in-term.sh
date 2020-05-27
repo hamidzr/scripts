5bab4d0db20e883e7914f31b56b5e41bfa69f302
Hamid Zare <dellydela@gmail.com>
2020-05-27 13:58:31 -0700

    generalize to run-in-terminal

--- run-in-term.sh
+++ run-in-term.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# run in terminal
+# $TERMINAL -e "sh -c '${@}'"
+$TERMINAL -e "sh -c '${1}'"
+
 # open a directory in your terminal emulator
 # $TERMINAL -e "cd ${dir} && zsh"
 
 # a more widely supported way of opening various terminals in a target dir
-$TERMINAL -e "sh -c 'cd $1 && $SHELL'"
+# $TERMINAL -e "sh -c 'cd $1 && $SHELL'"
+
