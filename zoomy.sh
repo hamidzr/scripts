f8aedb13764f672b1b979332e2f8d94203001472
Hamid Zare <dellydela@gmail.com>
2020-06-08 19:23:01 -0700

    update zoomy to support more Zoom links

--- zoomy.sh
+++ zoomy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-conf_num=$(echo "${1}" | grep zoom.us | grep -ioP '(?<=/j/)\d+')
+# link samples
+# https://applications.zoom.us/event/callback/slack/96111111113?startUrl=alsdjfls-asdl-alsjdf
 
-set -e
-# [[ -z $conf_num ]] && exit 1
+# conf_num=$(echo "${1}" | grep zoom.us | grep -ioP '(?<=/j/)\d+')
+conf_num=$(echo "${1}" | grep zoom.us | grep -ioP '\d{10,11}')
 
+[[ -z $conf_num ]] && exit 1
+
+command -v notify-send >/dev/null && notify-send "Joining zoom meeting: $conf_num"
 xdg-open "zoommtg://zoom.us/join?action=join&confno=$conf_num"
