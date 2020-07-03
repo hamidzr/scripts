c278b44457c368f25eba59911e3903cd30dc9a76
Hamid Zare <hamidzare.vu@gmail.com>
2020-07-02 19:30:20 -0700

    show the default device up top

--- media-control/switch-sink.sh
+++ media-control/switch-sink.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 
 sink_id=$1
 
 if [[ -z $sink_id ]]; then
-  sinks=$(pacmd list-sinks | grep -Pio '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n')
-  selected=$(echo "${sinks}" | dmenu -p 'Pick a sink/output/speaker' -i)
+  sinks=$(pacmd list-sinks | grep -Pi '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n')
+  default_sink=$(echo ${sinks} | grep '*' | grep -Pio '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n')
+  other_sinks=$(echo ${sinks} | grep -v '*' | grep -Pio '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n')
+  selected=$(echo -e "${default_sink}\n${other_sinks}" | dmenu -p 'Pick a sink/output/speaker' -i)
   sink_id=$(awk '{print $1}' <<< $selected)
 fi
 
--- media-control/switch-source.sh
+++ media-control/switch-source.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 
 source_id=$1
 
 if [[ -z $source_id ]]; then
-  sources=$(pacmd list-sources | grep -Pio '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n' | grep -v "Monitor of")
-  selected=$(echo "${sources}" | dmenu -p 'Pick a source/input/mic' -i)
+  sources=$(pacmd list-sources | grep -Pi '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n' | grep -v "Monitor of")
+  default_source=$(echo ${sources} | grep '*' | grep -Pio '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n')
+  other_sources=$(echo ${sources} | grep -v '*' | grep -Pio '((?<=device.description = )".*"|(?<=index: )\d+)' | paste -s -d ' \n')
+  selected=$(echo -e "${default_source}\n${other_sources}" | dmenu -p 'Pick a source/input/mic' -i)
   source_id=$(awk '{print $1}' <<< $selected)
 fi
 
