83d4cb1027794336216921c3620b80bdc3fadd6e
Hamid Zare <hamidzare.vu@gmail.com>
2020-09-08 22:41:16 -0700

    load shell env for starting zoom

--- zoomy.sh
+++ zoomy.sh
@@ -8,5 +8,7 @@ conf_num=$(echo "${1}" | grep zoom.us | grep -ioP '\d{10,11}')
 
 [[ -z $conf_num ]] && exit 1
 
+[ -f $HOME/.zshrc ] && source $HOME/.zshrc
+
 command -v notify-send >/dev/null && notify-send "Joining zoom meeting: $conf_num"
 xdg-open "zoommtg://zoom.us/join?action=join&confno=$conf_num"
