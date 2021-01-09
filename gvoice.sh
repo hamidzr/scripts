0145cbc36be640ceffb83db1e83bd18a3973e6d0
Hamid Zare <hamidzare.vu@gmail.com>
2021-01-09 13:12:59 -0800

    call through gvoice

---
+++ gvoice.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+country_code=1
+
+# input is expected without country code
+
+if [ -z "$1" ]; then
+    input=$(xsel -p) # or -b
+else
+    input="${@}"
+fi
+
+tel=$(echo $input | sed -e 's/-//g' -e 's/ //g' -e 's/(//g' -e 's/)//g' -e 's/\.//g' | grep -o '\([0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}\)\|\(([0-9]\{3\})[0-9]\{3\}\-[0-9]\{4\}\)\|\([0-9]\{10\}\)\|\([0-9]\{3\}\s[0-9]\{3\}\s[0-9]\{4\}\)')
+
+url=https://voice.google.com/u/0/calls?a=nc,%2B${country_code}${tel}
+
+# echo $tell
+
+xdg-open $url
