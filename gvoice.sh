01a553063c2a7a615b1a001e13e63bdf802d15e6
Hamid Zare <hamidzare.vu@gmail.com>
2021-01-12 10:12:19 -0800

    notify if no tel is found

--- gvoice.sh
+++ gvoice.sh
@@ -12,6 +12,11 @@ fi
 
 tel=$(echo $input | sed -e 's/-//g' -e 's/ //g' -e 's/(//g' -e 's/)//g' -e 's/\.//g' | grep -o '\([0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}\)\|\(([0-9]\{3\})[0-9]\{3\}\-[0-9]\{4\}\)\|\([0-9]\{10\}\)\|\([0-9]\{3\}\s[0-9]\{3\}\s[0-9]\{4\}\)')
 
+if [ -z "$tel" ]; then
+    notify-send "no phone number found"
+    exit 1
+fi
+
 url=https://voice.google.com/u/0/calls?a=nc,%2B${country_code}${tel}
 
 # echo $tell
